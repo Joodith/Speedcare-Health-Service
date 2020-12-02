5d9cf653415e54255ac48c35d967c3533901b4a4
Joodith <joodithl@gmail.com>
2020-12-02 21:23:03 +0530

    done

--- online/settings.py
+++ online/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '_0=-!z_ld^44f35fzd8p5yy!k7j^itmkd3kb(8kk_86n7rd#j1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['trial4-env.eba-wjnfspr7.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['trial5-env.eba-ccza5tms.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
